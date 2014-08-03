d52ce6914f65e261b37f450d9237679f46cd9a40
Joseph <josevem78@gmail.com>
2014-08-03 03:56:30 -0400

    JQ Radio

---
+++ js/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  
+
+function newSong() {
+$('.addTo').keydown(function (enter) {
+  if (enter.keyCode == 13) {
+      addSong();
+    }
+  });
+}
+
+newSong();
+    
+function addSong() {
+    var txtBox = $('.addTo').val();
+    var txtInput = '<li class="songs"><div class="holder"><img class="add" src="../images/confirm.png" ></div><span>' + txtBox + '</span>' +
+                    '<img class="remove" src="../images/delete.png" ></li>';
+    $('.playlist').prepend(txtInput);
+    $('.addTo').val(' '); 
+  }
+     
+});
+
+
+$(document).on('click','.add',function() {
+    $(this).closest('li').toggleClass('highlight');
+});
+
+
+$(document).on('click','.remove',function() {
+    $(this).closest('li').remove();
+});
